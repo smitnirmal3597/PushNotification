eb040028c5dfddd4fb823bcdc298ab67d8267219
Smit Nirmal <smitnirmal3597@gmail.com>
2019-09-14 00:34:36 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
+
+import { Router } from '@angular/router';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +18,72 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public push: Push,
+    public alertCtrl: AlertController,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      //this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.initPushNotification();
     });
   }
+
+  async initPushNotification(){
+
+    const options: PushOptions = {
+      android: {
+        senderID: environment.projectId,
+        //forceShow: true,
+        vibrate: true
+      }
+    };
+  
+    const pushObject: PushObject = this.push.init(options);
+  
+      pushObject.on('registration').subscribe(async (data: any) => {
+        console.log("device token:", data.registrationId);
+  
+        let alert = await this.alertCtrl.create({
+          header: 'device token',
+          subHeader: data.registrationId,
+          buttons: ['OK']
+        });
+        await alert.present();
+  
+      });
+  
+      pushObject.on('notification').subscribe(async (data: any) => {
+        console.log(data);
+        console.log('message', data.message);
+        if (data.additionalData.foreground) {
+          let confirmAlert = await this.alertCtrl.create({
+            header: 'New Notification',
+            message: data.message,
+            buttons: [{
+              text: 'Ignore',
+              role: 'cancel'
+            }, {
+              text: 'View',
+              handler: () => {
+                //TODO: Your logic here
+              }
+            }]
+          });
+          confirmAlert.present();
+        } 
+        else {
+          console.log('not inside app')
+          this.router.navigateByUrl('/home');
+          console.log("Push notification clicked");
+        }
+     });
+  
+    pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Push } from '@ionic-native/push/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Push,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
